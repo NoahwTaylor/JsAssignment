3b5bbfc275f12e03a0471a0fa4462dd07b7c2048
NoahwTaylor <noah.taylor1@edu.sait.ca>
2024-12-04 16:51:39 -0700

    t

--- scripts/contact.js
+++ scripts/contact.js
@@ -7,7 +7,7 @@ submit_button.addEventListener("click", formSubmitted)
 
 function formSubmitted(){
     let contact_page = document.getElementById('contact-page')
-    contact_page.innerHTML = <p>Thank you for your message</p>
+    contact_page.innerHTML = "Thank you for your message"
 }
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
