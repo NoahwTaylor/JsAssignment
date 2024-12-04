de423e6faa7ffdcc9460d35b007b7f0c14754bf0
NoahwTaylor <noah.taylor1@edu.sait.ca>
2024-12-04 16:50:35 -0700

    t

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,13 @@
-// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 fontlet contact_page = get
 
+
+let submit_button = document.getElementById('submit-button')
+
+submit_button.addEventListener("click", formSubmitted)
+
+function formSubmitted(){
+    let contact_page = document.getElementById('contact-page')
+    contact_page.innerHTML = <p>Thank you for your message</p>
+}
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
