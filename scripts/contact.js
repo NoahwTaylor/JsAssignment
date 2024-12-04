27d84f3c30c0b95bcab5fba9b309a6657de6f5fe
NoahwTaylor <noah.taylor1@edu.sait.ca>
2024-12-04 16:53:26 -0700

    h

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,13 +1,15 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 fontlet contact_page = get
 
 
-let submit_button = document.getElementById('submit-button')
+let submit_button = document.getElementById('submit-button');
 
-submit_button.addEventListener("click", formSubmitted)
+submit_button.addEventListener("click", formSubmitted);
 
-function formSubmitted(){
-    let contact_page = document.getElementById('contact-page')
-    contact_page.innerHTML = "Thank you for your message"
+function formSubmitted() {
+    let contact_page = document.getElementById('contact-page');
+    contact_page.innerHTML = "<p>Thank you for your message!</p>";
+    contact_page.querySelector('p').style.fontSize = "24px";
+    contact_page.querySelector('p').style.textAlign = "center";
 }
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
